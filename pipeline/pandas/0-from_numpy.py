90acf1f2de8f9d65c7d74271dddd12b35b962833
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-06 10:43:51 +0000

    Fixed

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -9,6 +9,9 @@ This module creates np array and assign it to df
 
 
 def from_numpy(array):
+    '''
+    This function creates orders then assign them to dataframe as columns
+    '''
     orders = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     df = pd.DataFrame(array, columns=orders[:array.shape[1]])
     return df
