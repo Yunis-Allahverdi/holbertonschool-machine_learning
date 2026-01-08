6299d083494997435a7442adaf0e39de0ad81542
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 13:21:04 +0000

    Fixed

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+This module sorts data
+'''
+
+
+def high(df):
+    '''
+    This fumction does same thing like above
+    '''
+    return df.sort_values(by='High', ascending=False)
