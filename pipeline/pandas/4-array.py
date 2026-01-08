552f9a13b0bca75f9cd9546fd6c550de27628bc2
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 11:53:33 +0000

    Fixed

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -4,13 +4,13 @@
 This module selects last 10 rows and
 converts them into numpy array
 '''
-import pandas as pd
+# import pandas as pd
 
 
 def array(df):
     '''
     This fumction does same thing like above
     '''
-    df = df['High', 'Close'].tail(10)
-    numpy_array = df.to_numpy()
+    dt = df['High', 'Close'].tail(10)
+    numpy_array = dt.to_numpy()
     return numpy_array
