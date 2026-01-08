d49a49eb46ccc5176e02968df8e651fdd8913a9f
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 17:59:26 +0000

    Fixed

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+This module joins two dataset
+'''
+import pandas as pd
+index = __import__('11-concat').index
+
+
+def hierarchy(df1, df2):
+    '''
+    Same with function
+    '''
+    df1 = index(df1)
+    df2 = index(df2)
+    df = pd.concat([df2_selected, df1], keys=["bitstamp", "coinbase"])
+    df = df.loc[1417411980:1417417980]
+    return df
