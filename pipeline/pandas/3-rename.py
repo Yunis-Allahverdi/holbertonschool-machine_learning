74d0f155b42e74171447a8d66a6db214e3d70569
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 11:42:31 +0000

    Fixed

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -11,6 +11,6 @@ def rename(df):
     This function loads data with delimiter
     '''
     df = df.rename(columns={"Timestamp": "Datetime"})
-    df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
     new_df = df[['Datetime', 'Close']]
     return new_df
