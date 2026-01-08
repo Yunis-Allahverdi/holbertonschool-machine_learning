9bc4c9e10baa2643a3cf11dadb18b1bbe1b06c72
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 14:21:29 +0000

    Fixed

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -13,5 +13,5 @@ def concat(df1, df2):
     '''
     df1 = index(df1)
     df2 = index(df2)
-    res = pd.concat([df1, df2], keys=["coinbase", "bitstamp"])
+    res = pd.concat([df2, df1], keys=["coinbase", "bitstamp"])
     return res
