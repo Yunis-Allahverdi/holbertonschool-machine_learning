b5d8e1aa9b12dee379c1418d81bc7853c5b469ea
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 18:10:28 +0000

    Fixed

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -10,8 +10,8 @@ def hierarchy(df1, df2):
     '''
     Same with function
     '''
-    df1 = index(df1)
-    df2 = index(df2)
+    df1 = df1.set_index('Timestamp')
+    df2 = df2.set_index('Timestamp')
     df1 = df1.loc[1417411980:1417417980]
     df2 = df2.loc[1417411980:1417417980]
     df = pd.concat([df2, df1], axis=0, keys=["bitstamp", "coinbase"])
