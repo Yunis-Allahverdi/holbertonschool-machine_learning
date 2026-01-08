ae0e312dd689e6ec0ebff4a50de8b15987d497f3
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 12:52:58 +0000

    Fixed

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -11,8 +11,5 @@ def slice(df):
     This fumction does same thing like above
     '''
     df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
-    new_df = pd.DataFrame(columns=['High', 'Low', 'Close', 'Volume_(BTC)'])
-    for i in range(0, len(df), 60):
-        new_df.loc[len(new_df)] = df.loc[i]
-
+    new_df = [df[i] for i in range(0, len(df), 60)]
     return new_df
