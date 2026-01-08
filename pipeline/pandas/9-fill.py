09781677aa1a3b8f311899808c2619df082812a3
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 13:41:11 +0000

    Fixed

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -14,6 +14,6 @@ def fill(df):
     df['High'] = df['High'].fillna(df['Close'])
     df['Low'] = df['Low'].fillna(df['Close'])
     df['Open'] = df['Open'].fillna(df['Close'])
-    df['Volume_(BTC)'].fillna(0)
-    df['Volume_(Currency)'].fillna(0)
+    df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
     return df
