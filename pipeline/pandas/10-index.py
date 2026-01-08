08ce72eaf5629fed6d6b1fd77e944a40add1494c
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 13:43:47 +0000

    Fixed

---
+++ pipeline/pandas/10-index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+This module sets index
+'''
+
+
+def fill(df):
+    '''
+    This fumction does same thing like above
+    '''
+    df.set_index('Timestamp')
+    return df
