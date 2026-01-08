ee3b36e0ee51931658ebd2e74a5fc5dbfc2986cf
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-08 13:31:32 +0000

    Fixed

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 '''
-This module sorts data
+This module fills data
 '''
 
 
-def high(df):
+def fill(df):
     '''
     This fumction does same thing like above
     '''
-    return df.sort_values(by='High', ascending=False)
+    df.drop('Weighted_Price', axis=1)
+    df['Close'].fillna(method='pad')
+    df['High'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
+    df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'].fillna(0)
+    return df
