dcfae26ec3d3906d686ab13bbf70b4d1e039b7a5
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-09 13:25:35 +0000

    Fixed

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+This module plots histogram of student scores by setting bins as 10 units
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    '''
+    This function does same thing as above
+    '''
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, 10, edgecolor = 'black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
