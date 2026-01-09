c45a0ddc2e7df2e9e64fe645fc369055957d5eb9
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-09 13:33:07 +0000

    Fixed

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,7 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, 10, edgecolor = 'black')
+    plt.hist(student_grades, 9, edgecolor = 'black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
