34461cbad025936677b1359c89d93e1f16bc02e0
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-09 12:34:39 +0000

    Fixed

---
+++ math/plotting/0-line.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+This module plots simple line
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    '''
+    This function does same thing as above
+    '''
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(np.arrange(0, 11), y)
+    plt.show()
