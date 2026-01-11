3dea06342622e8ba4c58e3f5814403d19cab9049
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-11 12:44:30 +0000

    Fixed

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -14,10 +14,14 @@ def bars():
     fruit = np.random.randint(0, 20, (4,3))
     #plt.figure(figsize=(6.4, 4.8)) 
     names = ['Farrah', 'Fred', 'Felicia']
-    plt.bar(names, fruit[0, :], label='apple', width=0.5, color='red')
-    plt.bar(names, fruit[1, :], label='bananas', bottom=column1, width=0.5, color='yellow')
-    plt.bar(names, fruit[2, :], label='oranges', bottom=column1+column2, width=0.5, color='#ff8000')
-    plt.bar(names, fruit[3, :], label='peaches', bottom=column1+column2+column3, width=0.5, color='#ffe5b4')
+    column1 = fruit[0, :]
+    column2 = fruit[1, :]
+    column3 = fruit[2, :]
+    column4 = fruit[3, :]
+    plt.bar(names, column1, label='apple', width=0.5, color='red')
+    plt.bar(names, column2, label='bananas', bottom=column1, width=0.5, color='yellow')
+    plt.bar(names, column3, label='oranges', bottom=column1+column2, width=0.5, color='#ff8000')
+    plt.bar(names, column4, label='peaches', bottom=column1+column2+column3, width=0.5, color='#ffe5b4')
     plt.yticks(np.arange(0, 81, 10))
     plt.legend()
     plt.title('Number of Fruit per Person')
