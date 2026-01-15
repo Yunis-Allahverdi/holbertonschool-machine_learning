0814d8b6295fef32a76a5dd25ae787d4e091d697
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-15 10:34:55 +0000

    Fixed

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,8 +6,9 @@ This module calc the derivative of poly
 
 
 def poly_derivative(poly):
-    if (not isinstance(poly, list)
-    or not all(isinstance(c, (int, float)) for c in poly)):
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(c, (int, float)) for c in poly):
         return None
 
     poly_der = []
