a05d031889452cab7ad710a8dc98f11df00c1a4b
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-15 10:57:40 +0000

    Fixed

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+'''
+This module calc the integral of poly
+'''
+
+
+def poly_integral(poly, C=0):
+    '''
+    Does same thing as above
+    '''
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(c, (int, float)) for c in poly):
+        return None
+    if len(poly) == 0:
+        return None
+    if not isinstance(C, (int, float)):
+        return None
+
+    poly_int = [C]
+
+    for i in range(len(poly)):
+        poly_int.append(poly[i] / (i + 1))
+
+    if len(poly_int) == 0:
+        return None
+    return poly_int
