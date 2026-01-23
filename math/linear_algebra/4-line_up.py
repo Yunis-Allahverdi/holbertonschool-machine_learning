f95226faee19ca4eab689b9d0f8ae825180f53df
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 11:28:04 +0000

    Fixed

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Addition of matrix
+'''
+
+
+def add_arrays(arr1, arr2):
+    '''
+    Does same thing as above
+    '''
+    new_arr = [0] * len(arr1)
+    if len(arr1) == len(arr2):
+        for i in range(len(arr1)):
+            new_arr[i] = arr1[i] + arr2[i]
+
+    return new_arr
