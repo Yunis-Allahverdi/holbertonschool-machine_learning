2817bcb3ce98b50b96d154e3754001c89b749713
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 13:06:06 +0000

    Fixed

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -11,7 +11,7 @@ def add_matrices2D(mat1, mat2):
     rows = len(mat1)
     cols = len(mat1[0])
     new_mat = [[0 for _ in range(cols)] for _ in range(rows)]
-    if len(mat1) == len(mat2):
+    if len(mat1[0]) == len(mat2[0]):
         for i in range(rows):
             for j in range(cols):
                 new_mat[i][j] = mat1[i][j] + mat2[i][j]
