c76a6309906d9c08d06e184966af2bb41bbb4f17
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 15:02:36 +0000

    Fixed

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Concat of matrix using np
+'''
+
+
+def np_cat(mat1, mat2, axis=0):
+    '''
+    Does same thing as above
+    '''
+    return np.concatenate((mat1, mat2), axis=axis)
