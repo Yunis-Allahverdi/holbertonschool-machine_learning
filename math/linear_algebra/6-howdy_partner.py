239e9b6b907cc531e6dae8b83a82b14378a712a2
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 13:11:59 +0000

    Fixed

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Concatenation of matrix
+'''
+
+
+def cat_arrays(arr1, arr2):
+    '''
+    Does same thing as above
+    '''
+    new_list = arr1
+    for i in arr2:
+        new_list.append(i)
+
+    return new_list
