5de074095e1e3bb6d83bc0764169de807467f889
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 14:46:41 +0000

    Fixed

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Calc of matrix using np
+'''
+
+
+def np_elementwise(mat1, mat2):
+    '''
+    Does same thing as above
+    '''
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
+    return add, sub, mul, div
