d5d7678ba20f458bdca376df80cb720427f893f0
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 14:38:32 +0000

    Fixed

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Transpose of matrix
+'''
+
+
+def np_transpose(matrix):
+    '''
+    Does same thing as above
+    '''
+    matrix_T = matrix.transpose()
+    return matrix_T
