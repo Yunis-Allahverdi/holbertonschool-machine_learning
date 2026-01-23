50aac32d572d924b582d04aa3d442712012ee914
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-23 15:05:57 +0000

    Fixed

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Matmul of matrix using np
+'''
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''
+    Does same thing as above
+    '''
+    return np.matmul(mat1, mat2)
