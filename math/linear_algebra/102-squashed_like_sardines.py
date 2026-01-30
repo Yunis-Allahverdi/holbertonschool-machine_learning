e54d459a9ded87dba48813e1b0428e64ea57b7d9
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-01-30 13:55:32 +0000

    Fixed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -8,20 +8,27 @@ def cat_matrices(mat1, mat2, axis=0):
     '''
     Does same thing as above
     '''
-    if axis == 0:
-        if not isinstance(mat1, list) or not isinstance(mat2, list):
+    if axis != 0:
+        if len(mat1) != len(mat2):
             return None
-        if len(mat1) > 0 and len(mat2) > 0:
-            if isinstance(mat1[0], list) != isinstance(mat2[0], list):
+
+    # Base Case: Concatenate at the target axis
+    if axis == 0:
+        # Check if the internal structures match (rank check)
+        # We ensure that if one is a list, the other is too,inner shapes match
+        m1, m2 = mat1, mat2
+        while isinstance(m1, list) and isinstance(m2, list):
+            if len(m1) != len(m2) and axis != 0:  # This part handled recursion
                 return None
+            if not m1 or not m2:
+                break
+            m1, m2 = m1[0], m2[0]
+        if isinstance(m1, list) != isinstance(m2, list):
+            return None
+
         return mat1 + mat2
 
-    if not isinstance(mat1, list) or not isinstance(mat2, list):
-        return None
-
-    if len(mat1) != len(mat2):
-        return None
-
+    # Recursive Step: Dive into the next dimension
     res = []
     for i in range(len(mat1)):
         sub_res = cat_matrices(mat1[i], mat2[i], axis - 1)
