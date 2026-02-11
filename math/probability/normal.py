4abb5bdf6543872cf22c7f505f2bb644971d6a94
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-02-11 12:52:45 +0000

    Fixed

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -47,3 +47,9 @@ class Normal:
         Doc
         '''
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        pi = 3.141592653589793
+        e = 2.7182818285
+        return (1 / (self.stddev * (2 * pi) ** 0.5)) * e **
+    (-0.5 * ((x - self.mean) / self.stddev) ** 2)
