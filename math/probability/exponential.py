b89be43de31691f62ff618b6a6bbb11bd026aae7
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-02-11 12:11:04 +0000

    Fixed

---
+++ math/probability/exponential.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+Documented
+'''
+
+
+class Exponential:
+    def __init__(self, data=None, lambtha=1.):
+        if data is not None:
+            if isinstance(data, list):
+                if len(data) < 2:
+                    raise ValueError("data must contain multiple values")
+                else:
+                    total = 0
+                    for i in data:
+                        total += i
+                    self.lambtha = 1 / float(total / len(data))
+            else:
+                raise TypeError("data must be a list")
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+    else:
+        self.lambtha = float(lambtha)
