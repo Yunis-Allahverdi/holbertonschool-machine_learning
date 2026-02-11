59688abd896997fac0e760da92a61b2c2b391ca3
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-02-11 13:09:52 +0000

    Fixed

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -32,3 +32,42 @@ class Binomial:
             p_temp = 1 - (variance / mean)
             self.n = int(round(mean / p_temp))
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        '''
+        Doc
+        '''
+
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        def factorial(num):
+            f = 1
+            for i in range(1, num + 1):
+                f *= i
+            return f
+
+        n_fact = factorial(self.n)
+        k_fact = factorial(k)
+        nk_fact = factorial(self.n - k)
+
+        combination = n_fact / (k_fact * nk_fact)
+        return combination * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        '''
+        Doc
+        '''
+
+        k = int(k)
+        if k < 0:
+            return 0
+        if k >= self.n:
+            return 1
+
+        total = 0
+        for i in range(k + 1):
+            total += self.pmf(i)
+
+        return total
