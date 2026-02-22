23adde3f53ba173acfddc1dd5b99e420c2c93250
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-02-22 15:10:46 +0000

    Fixed

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -12,11 +12,11 @@ def create_confusion_matrix(labels, logits):
 	Doc
 	'''
 
-    	classes = labels.shape[1]
-    	confusion = np.zeros((classes, classes))
-    	true_classes = np.argmax(labels, axis=1)
-    	pred_classes = np.argmax(logits, axis=1)
-    	for t, p in zip(true_classes, pred_classes):
-        	confusion[t, p] += 1
+    classes = labels.shape[1]
+    confusion = np.zeros((classes, classes))
+    true_classes = np.argmax(labels, axis=1)
+    pred_classes = np.argmax(logits, axis=1)
+    for t, p in zip(true_classes, pred_classes):
+        confusion[t, p] += 1
 
-    	return confusion
+    return confusion
