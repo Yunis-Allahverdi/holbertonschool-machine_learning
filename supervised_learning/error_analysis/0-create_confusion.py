4a40a1e2f65ac62a33f79769609d783d711c0d4a
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-02-25 05:58:55 +0000

    Fixed

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -4,15 +4,8 @@ Doc
 '''
 import numpy as np
 
-
 def create_confusion_matrix(labels, logits):
-	'''
-	Doc
     '''
-
-    classes = labels.shape[1]
-    y_true = np.argmax(labels, axis=1)
-    y_pred = np.argmax(logits, axis=1)
-    confusion = np.zeros((classes, classes))
-    np.add.at(confusion, (y_true, y_pred), 1)
-    return confusion
+    My function document
+    '''
+    return np.dot(labels.T, logits)
