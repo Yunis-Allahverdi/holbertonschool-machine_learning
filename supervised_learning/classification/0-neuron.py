c8429957eedc38e8cdc63ed6e534e49d27648980
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-08 14:18:25 +0000

    Fixed

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -13,6 +13,10 @@ class Neuron:
     '''
 
     def __init__(self, nx):
+        '''
+        Doc
+        '''
+
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
 
