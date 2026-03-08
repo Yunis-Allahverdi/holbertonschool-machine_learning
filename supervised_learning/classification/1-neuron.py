d96a0070234bad02db65ea79a2678be2886d2d87
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-08 14:29:27 +0000

    Fixed

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -26,12 +26,10 @@ class Neuron:
     def W(self):
         '''Getter for W'''
         return self.__W
-    
     @property
     def b(self):
         '''Getter for b'''
         return self.__b
-    
     @property
     def A(self):
         '''Getter for A'''
