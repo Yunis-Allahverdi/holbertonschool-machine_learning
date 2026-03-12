92333eda9808f269b036750c742ec691c6885b6f
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-12 16:45:52 +0000

    Fixed

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -7,12 +7,18 @@ import tensorflow.keras as K
 
 
 def save_config(network, filename):
+    '''
+    Doc
+    '''
     config = network.to_json()
     with open(filename, "w") as f:
         f.write(config)
 
 
 def load_config(filename):
+    '''
+    Doc
+    '''
     with open(filename, "r") as f:
         config = f.read()
 
