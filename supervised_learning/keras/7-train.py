72a7676fef1b659733bbaf498369dce6b360abd9
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-12 15:10:21 +0000

    Fixed

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+'''
+Documented
+'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    '''
+    Doc
+    '''
+    callbacks = []
+
+    if early_stopping and validation_data is not None:
+        early_stop = K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience,
+            restore_best_weights=True
+        )
+        callbacks.append(early_stop)
+    
+    if learning_rate_decay and validation_data is not None:
+        def lr_scheduler(epoch, lr):
+            new_lr = alpha / (1 + decay_rate * epoch)
+            print(f"\nEpoch {epoch+1}: Learning rate is {new_lr:.6f}.")
+            return new_lr
+
+        callbacks.append(K.callbacks.LearningRateScheduler(lr_scheduler, verbose=0))
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks
+    )
+
+    return history
