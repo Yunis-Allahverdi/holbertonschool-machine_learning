8d19bd011bc0450a9a7e15d39d529e8ce8f6b55b
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-12 12:34:22 +0000

    Fixed

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -15,8 +15,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model = [K.layers.Input(shape=(nx,))]
     for n, act in zip(layers, activations):
         model.append(
-                K.layers.Dense(n, activation=act, kernel_regularizer=K.regularizers.l2(lambtha))
+                K.layers.Dense(
+                    n,
+                    activation=act,
+                    kernel_regularizer=K.regularizers.l2(lambtha)
                 )
+        )
         model.append(K.layers.Dropout(rate=1-keep_prob))
 
     model.pop()
