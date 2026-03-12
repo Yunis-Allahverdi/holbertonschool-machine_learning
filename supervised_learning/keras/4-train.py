306b607bcb424837a227990b3f551ebfc2fee1ee
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-12 13:34:09 +0000

    Fixed

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+Documented
+'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return network
