463be5231d10e406625c868740e9042f9602847e
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-19 14:35:55 +0000

    Fixed

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -11,5 +11,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     Doc
     '''
     s_new = beta2 * s + (1 - beta2) * (grad ** 2)
-    var_new = var - alpha * ((grad) / (sqrt(s) + epsilon))
+    var_new = var - alpha * ((grad) / (np.sqrt(s_new) + epsilon))
     return var_new, s_new
