6cfbcbb48dcd3c02e8ef2c5fb6c34fc9808cba42
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-20 16:27:34 +0000

    Fixed

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+Doc
+'''
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    '''
+    Doc
+    '''
+    l2_term = 0.0
+
+    for layer in model.layers:
+        if hasattr(layer, 'kernel_regularizer') and
+        layer.kernel_regularizer is not None:
+            l2_term += layer.kernel_regularizer(layer.kernel)
+
+    total_cost = cost + l2_term
+    return total_cost
