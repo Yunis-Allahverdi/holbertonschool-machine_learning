2dcf0c3b83ef50388f5d24e4648070883d45e7ec
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-20 17:02:25 +0000

    Fixed

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+Doc
+'''
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    '''
+    Doc
+    '''
+    return tf.keras.layers.Dense(
+        n,
+        activation=activation,
+        kernel_regularizer=tf.keras.regularizers.L2(l2=lambtha)
+    )(prev)
