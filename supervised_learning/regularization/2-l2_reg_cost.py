d31a28c67bc724272428de011d358aeb6b830eca
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-29 14:14:51 +0000

    Fixed

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -10,7 +10,4 @@ def l2_reg_cost(cost, model):
     '''
     Doc
     '''
-    l2_losses = tf.add_n(model.losses)
-    total_cost = cost + l2_losses
-
-    return total_cost
+    return cost + tf.stack(model.losses)
