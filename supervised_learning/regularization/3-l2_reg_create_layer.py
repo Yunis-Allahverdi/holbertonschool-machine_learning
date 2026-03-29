f79a2970513a05ed21f412d61ce35c1b64ffbfd3
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-03-29 14:18:02 +0000

    Fixed

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -10,6 +10,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     '''
     Doc
     '''
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
