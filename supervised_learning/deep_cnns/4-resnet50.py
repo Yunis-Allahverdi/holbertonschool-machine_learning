0dd017c181e5f25b24dd266d6903e4cc07bb0d9a
Allahverdi-Yunis <allahverdiyunis@gmail.com>
2026-04-19 15:18:46 +0000

    Fixed

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Script to create an inception block"""
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
