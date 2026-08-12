da3a8ea0be28c600d1417c275dbb67955a5432cc
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-08-12 16:22:32 +0000

    fixed

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -7,12 +7,20 @@ class RNNCell:
     """Represents a cell of a simple RNN."""
 
     def __init__(self, i, h, o):
+        '''
+        Construction W and b
+        '''
+
         self.Wh = np.random.randn(h + i, h)
         self.Wy = np.random.randn(h, o)
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
+        '''
+        Creating forward pass
+        '''
+
         concat = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(concat @ self.Wh + self.bh)
         y = h_next @ self.Wy + self.by
