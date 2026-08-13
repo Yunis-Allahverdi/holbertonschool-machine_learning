79b6071d2aa2e4513d8039768abdd25e2ebd4871
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2026-08-13 16:59:40 +0000

    fixed

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-"""Trains a gensim Word2Vec model."""
+"""A module that creates a Word2Vec model"""
 import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates, builds and trains a Word2Vec model."""
-    sg = 0 if cbow else 1  # gensim: sg=0 is CBOW, sg=1 is skip-gram
-    model = gensim.models.Word2Vec(vector_size=vector_size,
-                                   min_count=min_count,
-                                   window=window,
-                                   negative=negative,
-                                   sg=sg,
-                                   seed=seed,
-                                   workers=workers,
-                                   epochs=epochs)
-    model.build_vocab(sentences)
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
+    """Create and train a Word2Vec model."""
+
+    model = gensim.models.Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=0 if cbow else 1,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
+
     return model
